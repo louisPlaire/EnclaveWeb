fc53996effd0579e68d6394a24c5e382dad98c30
Louis Plaire <99743964+louisPlaire@users.noreply.github.com>
2024-12-29 15:36:06 +0100

    fixed bugs

--- logic.js
+++ logic.js
@@ -1,6 +1,10 @@
 let homeBtn = document.getElementById("retour")
 
-
+if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    homeBtn.style.opacity = "1";
+}else{
+    homeBtn.style.opacity = "0";
+}
 
 
 window.onscroll = function(ev) {
